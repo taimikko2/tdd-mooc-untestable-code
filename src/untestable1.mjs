008f8ed45ad9d7abc38b8d56b09d9f9aaebc1ec3
Mikko Saarinen <el4302@gmail.com>
2023-01-29 16:26:20 +0200

    untestable 1 : date to parameter

--- src/untestable1.mjs
+++ src/untestable1.mjs
@@ -1,8 +1,9 @@
 const millisPerDay = 24 * 60 * 60 * 1000;
 
-export function daysUntilChristmas() {
-  const now = new Date();
+export function daysUntilChristmas(now) {
+  //const now = new Date();
   const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+  console.log("daysUntilChristmas("+now+") "+now.getFullYear()+"."+now.getMonth()+"."+now.getDate());
   const christmasDay = new Date(now.getFullYear(), 12 - 1, 25);
   if (today.getTime() > christmasDay.getTime()) {
     christmasDay.setFullYear(new Date().getFullYear() + 1);
@@ -10,3 +11,6 @@ export function daysUntilChristmas() {
   const diffMillis = christmasDay.getTime() - today.getTime();
   return Math.floor(diffMillis / millisPerDay);
 }
+
+// The assignment is to refactor the untestable code examples 
+// to make them testable, and then write tests for them.
--- test/untestable1.test.mjs
+++ test/untestable1.test.mjs
@@ -2,8 +2,38 @@ import { expect } from "chai";
 import { daysUntilChristmas } from "../src/untestable1.mjs";
 
 describe("Untestable 1: days until Christmas", () => {
-  it("todo", () => {
-    // TODO: write proper tests
-    expect(daysUntilChristmas()).to.be.a("number");
+  let day;
+  it("Current date is number", () => {
+    day = new Date();
+    expect(daysUntilChristmas(day)).to.be.a("number");
   });
+  
+  it("2020-12-24 gives 1", () => {
+    day = new Date('2020-12-24T00:00:00');
+    expect(daysUntilChristmas(day)).to.be.equal(1);
+
+    day = new Date('2020-12-24T23:59:50');
+    expect(daysUntilChristmas(day)).to.be.equal(1);
+  });
+
+  it("2020-12-25 gives 0", () => {
+    day = new Date('2020-12-25T00:00:00');
+    expect(daysUntilChristmas(day)).to.be.equal(0);
+
+    day = new Date('2020-12-25T23:59:50');
+    expect(daysUntilChristmas(day)).to.be.equal(0);
+  });
+
+  it("'March 1, 1995 03:24:00' gives 299", () => {
+    day = new Date('March 1, 1995 03:24:00');
+    expect(daysUntilChristmas(day)).to.be.equal(299);
+  });
+
+  it("2023-12-26 gives 365", () => {
+    day = new Date('2023-12-26T00:00:00');
+    expect(daysUntilChristmas(day)).to.be.equal(365);
+  });
+
 });
+
+// add day to parameter
