5a2222ec1d2bb5f6710b899343e741c166818a3b
Mikko Saarinen <el4302@gmail.com>
2023-01-30 11:50:23 +0200

    utetable 2: Randomness

--- test/untestable2.test.mjs
+++ test/untestable2.test.mjs
@@ -1,5 +1,5 @@
 import { expect } from "chai";
-import { diceHandValue , diceRoll} from "../src/untestable2.mjs";
+import { diceHandValue, diceRoll } from "../src/untestable2.mjs";
 
 function distinctNumbers(amount) {
   const distinct = new Set();
@@ -13,20 +13,25 @@ function distinctNumbers(amount) {
 
 describe("Untestable 2: a dice game", () => {
   it("value is a number", () => {
-    expect(diceHandValue(1,2)).to.be.a("number");
+    expect(diceHandValue(1, 2)).to.be.a("number");
   });
 
   it("diceHandValue is bigger of two digits", () => {
-    expect(diceHandValue(1,2)).to.be.equal(2);
+    expect(diceHandValue(1, 2)).to.be.equal(2);
   });
   it("diceHandValue is 100+number when numers are same", () => {
-        expect(diceHandValue(2,2)).to.be.equal(102);
+    expect(diceHandValue(2, 2)).to.be.equal(102);
+  });
+
+  it("diceRoll gives 6 different values", () => {
+    const numbers = distinctNumbers(100);
+    expect(numbers.size).to.equal(6);
   });
 
   it("diceRoll gives values netween 1 and 6", () => {
     const numbers = distinctNumbers(100);
-    expect(numbers.size).to.equal(6);
-    // expect them all be between 1 and 6
-    //expect(numbers).deep.contain.all(1,2,3,4,5,6);
+    numbers.forEach(function (value) {
+      expect(value).to.be.lte(6).and.gte(1);
+    });
   });
 });
