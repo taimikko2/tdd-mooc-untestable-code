bbed803ffa79d30ab7b111b1a05003b9cb153fda
Mikko Saarinen <el4302@gmail.com>
2023-01-30 09:15:44 +0200

    new tests

--- test/untestable2.test.mjs
+++ test/untestable2.test.mjs
@@ -3,15 +3,14 @@ import { diceHandValue } from "../src/untestable2.mjs";
 
 describe("Untestable 2: a dice game", () => {
   it("value is a number", () => {
-    // TODO: write proper tests
-    expect(diceHandValue()).to.be.a("number");
+    expect(diceHandValue(1,2)).to.be.a("number");
   });
 
-  xit("todo", () => {
-    expect(diceHandValue()).to.be.a("number");
+  it("diceHandValue is bigger of two digits", () => {
+    expect(diceHandValue(1,2)).to.be.equal(2);
   });
-  xit("todo", () => {
-        expect(diceHandValue()).to.be.a("number");
+  xit("diceHandValue is 100+number when numers are same", () => {
+        expect(diceHandValue(2,2)).to.be.equal(102);
   });
   xit("todo", () => {
     expect(diceHandValue()).to.be.a("number");
