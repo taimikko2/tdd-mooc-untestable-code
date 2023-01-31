f690a45deacb355b80b04295dc10b824243c4b64
Mikko Saarinen <el4302@gmail.com>
2023-01-31 08:40:29 +0200

    test for 'can parse several people'

--- test/untestable3.test.mjs
+++ test/untestable3.test.mjs
@@ -2,11 +2,6 @@ import { expect } from "chai";
 import { writeFile, unlink } from "node:fs/promises";
 import { parsePeopleCsv } from "../src/untestable3.mjs";
 
-// example input:
-// Loid,Forger,,Male
-// Anya,Forger,6,Female
-// Yor,Forger,27,Female
-
 const file1 = "untestable3-1.csv";
 const file2 = "untestable3-2.csv";
 const file3 = "people.csv";
@@ -15,13 +10,13 @@ describe("Untestable 3: CSV file parsing", () => {
     let data = `Jaska,Jokunen,6,Male`;
     writeFile(file1, data, (error) => {
       if (error) {
-        console.error("ERROR: untestable3-1.csv "+error);
+        console.error("ERROR: untestable3-1.csv " + error);
         throw err;
       }
     });
     writeFile(file2, data, (error) => {
       if (error) {
-        console.error("ERROR: untestable3-2.csv "+error);
+        console.error("ERROR: untestable3-2.csv " + error);
         throw err;
       }
     });
@@ -58,32 +53,21 @@ describe("Untestable 3: CSV file parsing", () => {
     }
   });
 
-  xit("can parse several people", async () => {
-    // TODO: write proper tests
-    let res = await parsePeopleCsv(file3);
-    let person;
-    for (let i = 0; i < res.length; i++) {
-      person = res[i];
-      console.log("it can parse People Csv " + person.firstName + " " + person.lastName + " " + person.age);
-    }
-    res.forEach(function (value, key) {
-      console.log("for each " + JSON.stringify(key) + " = " + JSON.stringify(value));
-    });
+  it("can parse several people", async () => {
     try {
-      // await parsePeopleCsv("people.csv"))
-      expect(res).to.deep.equal([{ firstName: "Jaska", lastName: "Jokunen", gender: "male", age: 6 }]);
+      const res = await parsePeopleCsv(file3);
+      expect(res).to.deep.equal([
+        { firstName: "Loid", lastName: "Forger", gender: "m" },
+        { firstName: "Anya", lastName: "Forger", age: 6, gender: "f" },
+        { firstName: "Yor", lastName: "Forger", age: 27, gender: "f" },
+      ]);
     } catch (e) {
-      console.error("Virhe " + e);
-      console.log(e.firstName + " " + e.lastName + " " + e.age + " " + e.gender);
+      expect(e).not.to.exist;
+      console.log("ERROR: can parse several people " + e);
+    } finally {
+      unlink(file3);
     }
   });
-
-  // skip empty lines
-
-  // trim
-
-  unlink(file1);
-  unlink(file2);
 
 });
 
