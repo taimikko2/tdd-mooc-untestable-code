410122edb196542b9c862f4e9c0f6e79c8124abf
Mikko Saarinen <el4302@gmail.com>
2023-02-02 16:43:46 +0200

    test for password can be changed

--- test/untestable4.test.mjs
+++ test/untestable4.test.mjs
@@ -4,18 +4,14 @@ import argon2 from "@node-rs/argon2";
 
 describe("Untestable 4: enterprise application", () => {
   let service;
-  //let pgDao;
   const user = { userId: 1, passwordHash: "kissa" };
   const user2 = { userId: 2, passwordHash: "koira" };
-  //var salt = await argon2.generateSalt(); // = argon2.generateSaltSync(); // "1234567890"; //
 
   beforeEach(() => {
-    //pgDao = new PostgresUserDao();
     service = new PasswordService("vähemmän suolaa");
   });
 
   afterEach(() => {
-    //pgDao.close();
     service.users.close();
   });
 
@@ -28,23 +24,23 @@ describe("Untestable 4: enterprise application", () => {
   it("password can be hashed ja verified", async () => {
     const word = "koira";
     let hash = await service.hashPassword(word);
-    console.log(" hash " + word + " on " + hash);
+    //console.log(" hash " + word + " on " + hash);
     let verified = await service.verifyHash(hash, word);
-    console.log("verified = "+verified);
+    //console.log("verified = "+verified);
     expect(verified).to.be.true;
   });
 
-  xit("password can be changed", async () => {
-    let tmp;
-    tmp = argon2.hashSync("koira", salt);
-    user2.passwordHash = tmp;
-    console.log("hash " + tmp);
-    return;
-    await service.users.save(user2);
-    let newPassword = "changed" + user2.passwordHash;
-    console.log("viedään käyttäjä: " + JSON.stringify(user) + " new pw: " + newPassword);
-    await service.changePassword(user.userId, user.passwordHash, newPassword);
-    user.passwordHash = newPassword;
+  it("password can be changed", async () => {
+    const pwd = "koira";
+    let hash = await service.hashPassword(pwd);
+    user2.passwordHash = hash;
+    console.log("talletetaan user2 " + JSON.stringify(user2));
+    await service.save(user2);
+    const newPwd = "toinen " + pwd;
+    hash = await service.hashPassword(newPwd);
+//    console.log("viedään käyttäjä: " + JSON.stringify(user2) + " new pw: " + newPassword);
+    await service.changePassword(user.userId, user.passwordHash, newPwd);
+    user2.passwordHash = hash;
     let u2 = await service.users.getById(1);
     console.log("saadaan käyttäjä: " + JSON.stringify(u2));
     expect(u2).to.deep.equal(user);
