66b3303f156d042b1a28d0f4e907e91e68c6b798
Mikko Saarinen <el4302@gmail.com>
2023-02-03 10:51:32 +0200

    cleaner test for changing password

--- test/untestable4.test.mjs
+++ test/untestable4.test.mjs
@@ -24,26 +24,21 @@ describe("Untestable 4: enterprise application", () => {
   it("password can be hashed ja verified", async () => {
     const word = "koira";
     let hash = await service.hashPassword(word);
-    //console.log(" hash " + word + " on " + hash);
     let verified = await service.verifyHash(hash, word);
-    //console.log("verified = "+verified);
     expect(verified).to.be.true;
   });
 
   it("password can be changed", async () => {
     const pwd = "koira";
-    let hash = await service.hashPassword(pwd);
-    user2.passwordHash = hash;
-    console.log("talletetaan user2 :    " + JSON.stringify(user2) + " old pw: " + pwd);
-    await service.save(user2);
     const newPwd = "toinen " + pwd;
-    let newHash = await service.hashPassword(newPwd);
-    console.log("kohta change Password: " + JSON.stringify(user2) + " new pw: " + newPwd);
+    user2.passwordHash = await service.hashPassword(pwd);
+    const newHash = await service.hashPassword(newPwd);
+
+    await service.save(user2);
     await service.changePassword(user2.userId, pwd, newPwd);
-    user2.passwordHash = hash;
-    let u2 = await service.getById(1);
-    console.log("saadaan käyttäjä: " + JSON.stringify(u2));
-    expect(u2).to.deep.equal(user);
+    let userChanged = await service.getById(user2.userId);
+    expect(userChanged.userId).to.equal(user2.userId); 
+    expect(await service.verifyHash(userChanged.passwordHash, newPwd)).to.be.true;
   });
 
   xit("can't change password if old password is wrong", async () => {
