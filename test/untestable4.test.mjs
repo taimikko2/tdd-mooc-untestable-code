08916246eda16af5cc856ae2853f905b0f3b2c22
Mikko Saarinen <el4302@gmail.com>
2023-02-03 10:54:27 +0200

    it("can't change password if old password is wrong"

--- test/untestable4.test.mjs
+++ test/untestable4.test.mjs
@@ -41,14 +41,17 @@ describe("Untestable 4: enterprise application", () => {
     expect(await service.verifyHash(userChanged.passwordHash, newPwd)).to.be.true;
   });
 
-  xit("can't change password if old password is wrong", async () => {
-    await service.users.save(user);
-    //console.log("viedään käyttäjä: " + JSON.stringify(user));
-    const newPassword = "changed " + user.passwordHash;
-    await service.changePassword(user.userId, user.passwordHash, newPassword);
-    user.passwordHash = newPassword;
-    let u2 = await service.users.getById(1);
-    //console.log("saadaan käyttäjä: " + JSON.stringify(u2));
-    expect(u2).to.deep.equal(user);
+  it("can't change password if old password is wrong", async () => {
+    const pwd = "koira";
+    const newPwd = "toinen " + pwd;
+    user2.passwordHash = await service.hashPassword(pwd);
+    const newHash = await service.hashPassword(newPwd);
+
+    await service.save(user2);
+    await service.changePassword(user2.userId, "kissa", newPwd);
+    let userChanged = await service.getById(user2.userId);
+    // virheilmoitus
+    expect(userChanged.userId).to.equal(user2.userId); 
+    expect(await service.verifyHash(userChanged.passwordHash, newPwd)).to.be.false;
   });
 });
